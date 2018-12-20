921b0a583892fcfd7e302afa7bc5a96ae65d337d
v4pic <45578618+v4pic@users.noreply.github.com>
2018-12-19 22:50:55 -0500

    Add files via upload

---
+++ online-library/inc/data.php
@@ -0,0 +1,161 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "The Miracle Morning",
+	"img" => "img/media/MIRACLE_MORNING_1.jpg",
+    "genre" => "Self-help",
+    "format" => "Paperback",
+    "year" => 2012,
+    "category" => "Books",
+    "authors" => [
+        "Hal Elrod",
+    ],
+    "publisher" => "Miracle Morning Publishing, LLC",
+    "isbn" => '9780979019715'
+];
+$catalog[102] = [
+    "title" => "7 Habits of Highly effective People",
+    "img" => "img/media/7-habits-hardcover-665x1024.png",
+    "genre" => "Self-help",
+    "format" => "Paperback",
+    "year" => 1989,
+    "category" => "Books",
+    "authors" => [
+        "Stephen Covey"
+    ],
+    "publisher" => "Free Press",
+    "isbn" => '9781471129391'
+];
+$catalog[103] = [
+    "title" => "The One Thing",
+    "img" => "img/media/one-thing.png",
+    "genre" => "Self-help",
+    "format" => "Paperback",
+    "year" => 2013,
+    "category" => "Books",
+    "authors" => [
+        "Gary W. Keller",
+				"Jay Papasan"
+    ],
+    "publisher" => "Bard Press",
+    "isbn" => '978-1885167774'
+];
+$catalog[104] = [
+    "title" => "The Science of Getting Rich",
+    "img" => "img/media/RICH.png",
+    "genre" => "Self-help",
+    "format" => "Paperback",
+    "year" => 1910,
+    "category" => "Books",
+    "authors" => [
+        "Wallace D. Wattles"
+    ],
+    "publisher" => "CreateSpace Independent Publishing Platform",
+    "isbn" => '1984052330'
+];
+//Movies
+$catalog[201] = [
+    "title" => "The Good the Bad and the Ugly",
+    "img" => "img/media/AAGBU.PNG",
+    "genre" => "Western",
+    "format" => "DVD",
+    "year" => 1967,
+    "category" => "Movies",
+    "director" => "Sergio Leone",
+    "writers" => [
+        "Sergio Leone",
+        "Luciano Vincenzoni"
+    ],
+    "stars" => [
+        "Clint Eastwood",
+	      "Eli Wallach",
+	      "Lee Van Cleef"
+    ]
+];
+$catalog[202] = [
+    "title" => "Return of the Jedi",
+    "img" => "img/media/ReturnOfTheJediPoster1983.jpg",
+    "genre" => "Fantasy",
+    "format" => "Blu-ray",
+    "year" => 1983,
+    "category" => "Movies",
+    "director" => "Richard Marquand",
+    "writers" => [
+        "George Lucas"
+    ],
+    "stars" => [
+        "Mark Hamill",
+	      "Harrison Ford",
+	      "Carrie Fisher",
+	      "Billy Dee Williams"
+    ]
+];
+$catalog[203] = [
+    "title" => "Raiders of the Lost Ark",
+    "img" => "img/media/indianajones.jpg",
+    "genre" => "Action",
+    "format" => "Blu-ray",
+    "year" => 1981,
+    "category" => "Movies",
+    "director" => "Steven Spielberg",
+    "writers" => [
+        "George Lucas",
+	      "Philip Kaufman"
+    ],
+    "stars" => [
+        "Harrison Ford",
+    ]
+];
+$catalog[204] = [
+    "title" => "Moonraker",
+    "img" => "img/media/Moonraker.png",
+    "genre" => "Action",
+    "format" => "DVD",
+    "year" => 1979,
+    "category" => "Movies",
+    "director" => "Lewis Gilbert",
+    "writers" => [
+        "Christopher Wood"
+    ],
+    "stars" => [
+        "Roger Moore"
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Achtung Baby",
+    "img" => "img/media/U2_Achtung_Baby_7.jpg",
+    "genre" => "Alternative Rock",
+    "format" => "CD",
+    "year" => 1990,
+    "category" => "Music",
+    "artist" => "U2"
+];
+$catalog[302] = [
+    "title" => "Meteora",
+    "img" => "img/media/linkinpark.png",
+    "genre" => "Alternative Rock",
+    "format" => "CD",
+    "year" => 2003,
+    "category" => "Music",
+    "artist" => "Linkin Park"
+];
+$catalog[303] = [
+    "title" => "There Is Nothing Left to Lose",
+    "img" => "img/media/foofighters2.jpg",
+    "genre" => "Alternative Rock",
+    "format" => "CD",
+    "year" => 1999,
+    "category" => "Music",
+    "artist" => "Foo Fighters"
+];
+$catalog[304] = [
+    "title" => "Violator",
+    "img" => "img/media/depechemode.png",
+    "genre" => "New Wave",
+    "format" => "CD",
+    "year" => 1990,
+    "category" => "Music",
+    "artist" => "Depeche Mode"
+];
---
+++ online-library/inc/footer.php
@@ -0,0 +1,19 @@
+</div>
+
+<div class="footer">
+
+  <div class="wrapper">
+
+    <ul>
+      <li><a href="https://www.instagram.com/laguardiacc">Instagram</a></li>
+      <li><a href="https://www.facebook.com/LaGuardiaCC">Facebook</a></li
+  </ul>
+
+  <p>&copy;<?php echo date("Y"); ?> My Online Library</p>
+
+</div>
+
+</div>
+
+</body>
+</html>
